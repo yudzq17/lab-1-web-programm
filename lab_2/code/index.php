0a494cbe3e0114c9d3c2997f73bba3ff2a861a9a
yudzq17 <harryvers@mail.ru>
2024-03-03 15:13:16 +0200

    1 task

--- lab_2/code/index.php
+++ lab_2/code/index.php
@@ -1,2 +1,12 @@
 <?php
-echo phpinfo();
+/* Imagine a lot of code here */
+$very_bad_unclear_name = "15 chicken wings";
+
+
+// Write your code here:
+$order =& $very_bad_unclear_name;
+$order.= " from kfc is the best dish";
+
+
+// Don't change the line below
+echo "\nYour order is: $very_bad_unclear_name.";
