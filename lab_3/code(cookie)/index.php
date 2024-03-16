ee2afd1af23246843c79e91056c459ebf3ed8e1d
yudzq17 <harryvers@mail.ru>
2024-03-16 18:58:23 +0200

    first task cookies

---
+++ lab_3/code(cookie)/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Word and Character Count</title>
+</head>
+<body>
+<form method="post">
+    <textarea name="text" rows="4" cols="50"></textarea><br>
+    <input type="submit" name="submit" value="Count">
+</form>
+
+<?php
+if(isset($_POST['submit'])){
+    if(!empty($_POST['text'])){
+        $text = $_POST['text'];
+        $word_count = str_word_count($text);
+        $char_count = strlen($text);
+
+        echo "Количесвто слов: $word_count<br>";
+        echo "Количесвтво букв: $char_count";
+    } else {
+        echo "Сначала напишите что-нибудь";
+    }
+}
+?>
+</body>
+</html>
